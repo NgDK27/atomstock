0e87a22d6f7006e287a29bfd755ac4c4729b5d16
Quân Đỗ <aloha.quando@gmail.com>
2024-06-26 15:55:41 +0700

    Completed most of Onboarding.

--- lib/pages/onboarding/widgets/onboarding_layout.dart
+++ lib/pages/onboarding/widgets/onboarding_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oppenhomies/styles/colors.dart';
@@ -9,6 +10,8 @@ import 'package:oppenhomies/widgets/buttons/primary/OpFilledGlowPrimaryButton.da
 import 'package:oppenhomies/widgets/buttons/primary/OpTonalPrimaryButton.dart';
 import 'package:oppenhomies/widgets/illustrations/glassmorphism_base.dart';
 import 'package:prevent_orphan_text/prevent_orphan_text.dart';
+
+import '../../../styles/cupertino_theme.dart';
 
 class OnboardingLayout extends ConsumerWidget {
   final String title;
@@ -30,74 +33,100 @@ class OnboardingLayout extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Stack(
-      children: [
-        ColorFiltered(
-            colorFilter: ColorFilter.mode(
-              platformThemeData(context,
-                  material: (ThemeData data) => data.colorScheme.primary
-                      .withOpacity(OpOpacity.quaternary),
-                  cupertino: (_) =>
-                      OpDynamicColor.primary.withOpacity(OpOpacity.quaternary)),
-              BlendMode.srcOver,
-            ),
-            child: ColorFiltered(
+    final PlatformTarget currentPlatform = platform(context);
+
+    switch (currentPlatform) {
+      case PlatformTarget.iOS:
+        {
+          SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+        }
+      case PlatformTarget.android:
+        {
+          SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            systemNavigationBarColor: Colors.transparent,
+          ));
+        }
+      default:
+        {}
+    }
+
+    return PlatformWidgetBuilder(
+        material: (_, child, __) => Theme(
+            data: Theme.of(context).copyWith(brightness: Brightness.dark),
+            child: child!),
+        cupertino: (_, child, __) =>
+            CupertinoTheme(data: opCupertinoDarkTheme, child: child!),
+        child: Stack(
+          children: [
+            ColorFiltered(
                 colorFilter: ColorFilter.mode(
                   platformThemeData(context,
-                      material: (ThemeData data) => data.colorScheme.surface
+                      material: (ThemeData data) => data.colorScheme.primary
                           .withOpacity(OpOpacity.quaternary),
-                      cupertino: (_) => OpDynamicColor.surface
-                          .withOpacity(OpOpacity.tertiary)),
+                      cupertino: (CupertinoThemeData data) =>
+                          data.primaryColor.withOpacity(OpOpacity.quaternary)),
                   BlendMode.srcOver,
                 ),
-                child: ClipRect(
-                  child: Transform.scale(
-                    scale: backgroundScale,
-                    alignment: backgroundAlignment,
-                    child: Image.asset(
-                      'assets/images/onboarding_bg.png',
-                      fit: BoxFit.cover,
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
+                child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      platformThemeData(context,
+                          material: (ThemeData data) => data.colorScheme.surface
+                              .withOpacity(OpOpacity.quaternary),
+                          cupertino: (_) => OpColor.charcoal120
+                              .withOpacity(OpOpacity.tertiary)),
+                      BlendMode.srcOver,
+                    ),
+                    child: ClipRect(
+                      child: Transform.scale(
+                        scale: backgroundScale,
+                        alignment: backgroundAlignment,
+                        child: Image.asset(
+                          'assets/images/onboarding_bg.png',
+                          fit: BoxFit.cover,
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height,
+                        ),
+                      ),
+                    ))),
+            SafeArea(
+              minimum: const EdgeInsets.symmetric(horizontal: OpSpacing.md),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: OpSpacing.xl),
+                  PreventOrphanText(
+                    title,
+                    style: platformThemeData(context,
+                        material: (ThemeData data) =>
+                            data.textTheme.displaySmall?.copyWith(
+                                color: data.colorScheme.onPrimaryContainer),
+                        cupertino: (CupertinoThemeData data) => data
+                            .textTheme.navLargeTitleTextStyle
+                            .copyWith(color: OpColor.mono100)),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Transform.scale(
+                        scale: 1.2,
+                        child: illustration,
+                      ),
                     ),
                   ),
-                ))),
-        SafeArea(
-          minimum: const EdgeInsets.symmetric(horizontal: OpSpacing.md),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: OpSpacing.xl),
-              PreventOrphanText(
-                title,
-                style: platformThemeData(context,
-                    material: (ThemeData data) => data.textTheme.displaySmall
-                        ?.copyWith(color: data.colorScheme.onSurface),
-                    cupertino: (CupertinoThemeData data) =>
-                        data.textTheme.navLargeTitleTextStyle),
-              ),
-              Expanded(
-                child: Center(
-                  child: Transform.scale(
-                    scale: 1.2,
-                    child: illustration,
+                  const SizedBox(height: OpSpacing.md),
+                  OpTonalPrimaryButton(
+                    text: "Sign in",
+                    onPressed: onSignInPressed,
                   ),
-                ),
+                  const SizedBox(height: OpSpacing.sm),
+                  OpFilledGlowPrimaryButton(
+                    text: "Get started",
+                    onPressed: onGetStartedPressed,
+                  ),
+                ],
               ),
-              const SizedBox(height: OpSpacing.md),
-              OpTonalPrimaryButton(
-                text: "Sign in",
-                onPressed: onSignInPressed,
-              ),
-              const SizedBox(height: OpSpacing.sm),
-              OpFilledGlowPrimaryButton(
-                text: "Get started",
-                onPressed: onGetStartedPressed,
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
+            ),
+          ],
+        ));
   }
 }
