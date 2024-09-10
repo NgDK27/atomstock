9307cbfc7ce794354db9ec8c2874545236fadda1
alohaquando <services@quanhoangdo.com>
2024-09-10 18:45:13 +0700

    View all stocks on Explore done

--- lib/pages/market/screens/market.dart
+++ lib/pages/market/screens/market.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:oppenhomies/domain/models/stock/market_list/stock_market_stocks_model.dart';
@@ -7,6 +8,7 @@ import 'package:oppenhomies/domain/models/stock/stock_model.dart';
 import 'package:oppenhomies/domain/providers/stock/market/stock_explore.dart';
 import 'package:oppenhomies/navigation/routes.dart';
 import 'package:oppenhomies/styles/effects.dart';
+import 'package:oppenhomies/styles/spacings.dart';
 import 'package:oppenhomies/widgets/list_tiles/stock_list_tile.dart';
 import 'package:oppenhomies/widgets/scaffolds/platform_sliver_scaffold.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -32,29 +34,57 @@ class Market extends ConsumerWidget {
           sliver: SliverList(
             delegate: SliverChildBuilderDelegate(
               (context, idx) {
-                if (idx >=
+                if (idx == 0) {
+                  return _searchBar(context);
+                }
+
+                final stockIndex = idx - 1;
+                if (stockIndex >=
                     (allStocks.hasValue
                         ? allStocks.requireValue.stocks.length
                         : 10)) return null;
+
                 return Skeletonizer(
                   effect: opShimmerEffect(context),
                   enableSwitchAnimation: true,
                   enabled: !allStocks.hasValue,
                   child: StockListTile(
                     stock: allStocks.hasValue
-                        ? allStocks.requireValue.stocks[idx]
+                        ? allStocks.requireValue.stocks[stockIndex]
                         : StockModel.sample(),
                   ),
                 );
-                // return StockListTile(stock: StockModel.sample());
               },
-              childCount: allStocks.hasValue
-                  ? allStocks.requireValue.stocks.length
-                  : 10,
+              childCount: (allStocks.hasValue
+                      ? allStocks.requireValue.stocks.length
+                      : 10) +
+                  1,
             ),
           ),
         ),
       ],
+    );
+  }
+
+  Widget _searchBar(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(
+          OpSpacing.md, OpSpacing.none, OpSpacing.md, OpSpacing.md),
+      child: PlatformWidget(
+        cupertino: (_, __) => CupertinoSearchTextField(
+          placeholder: "Search for stocks and indexes",
+          onTap: () => navigateMarketSearch(context),
+        ),
+        material: (_, __) => SearchBar(
+          leading: const Padding(
+            padding: EdgeInsets.only(left: OpSpacing.xs),
+            child: Icon(Icons.search),
+          ),
+          hintText: "Search for stocks and indexes",
+          elevation: const MaterialStatePropertyAll(0),
+          onTap: () => navigateMarketSearch(context),
+        ),
+      ),
     );
   }
 }
